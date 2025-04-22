bc0f10fc76d0e03f2ba4f5dcc0c1662b7419c70a
Anastassiya Paramonova <paramonova.a.p.01@gmail.com>
2025-04-22 19:58:03 +0300

    fix5

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import { ThemeProvider, CssBaseline, Container } from '@mui/material';
-import theme from './theme';
-import "./assets/style.css"
+import React from "react";
+import { ThemeProvider, CssBaseline, Container } from "@mui/material";
+import theme from "./theme";
+import "./assets/style.css";
 
-import Navbar from './componens/Navbar';
-import HeroSection from './componens/HeroSection';
-import Portfolio from './componens/Portfolio';
-import Footer from './componens/Footer';
+import Navbar from "./componens/Navbar";
+import HeroSection from "./componens/HeroSection";
+import Portfolio from "./componens/Portfolio";
+import Footer from "./componens/Footer";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Navbar />
-      <Container maxWidth="lg">
-        <HeroSection />
-        <Portfolio />
-      </Container>
+
+      <HeroSection />
+      <Portfolio />
+
       <Footer />
     </ThemeProvider>
   );
 }
 
 export default App;
-
